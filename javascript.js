b5e89ede8ce29ebf80b21491b8a9eb8876bde7c4
Definispect <153238434+Definispect@users.noreply.github.com>
2024-09-18 20:07:07 -0400

    Change grid visualization, variable names
    Numbers changed to square grid points, variables for code clarity

--- javascript.js
+++ javascript.js
@@ -1,16 +1,17 @@
+const GRID_LENGTH_MAX = 16;
+
 const sketchBoard = document.querySelector(".sketch-board");
 
 let boardDivs = [[]];
 
-for (let rowNum = 0; rowNum < 16; rowNum++) {
+for (let rowNum = 0; rowNum < GRID_LENGTH_MAX; rowNum++) {
     boardDivs[rowNum] = document.createElement("div");
     boardDivs[rowNum].classList.add("columns");
     sketchBoard.appendChild(boardDivs[rowNum]);
 
-    for (let columnNum = 1; columnNum <= 16; columnNum++) {
+    for (let columnNum = 0; columnNum < GRID_LENGTH_MAX; columnNum++) {
         console.log(columnNum);
         boardDivs[rowNum][columnNum] = document.createElement("div");
-        boardDivs[rowNum][columnNum].innerText = `${columnNum}`;
         boardDivs[rowNum][columnNum].classList.add("gridPositions");
         boardDivs[rowNum].appendChild(boardDivs[rowNum][columnNum]);
     };
